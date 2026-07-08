31a9769c03eb39e0b2e472cf4efea949946fe283
Hendrisetyawaninformatika <setyawanhendri253@gmail.com>
2026-07-09 01:21:32 +0700

    Update idex

--- index.php
+++ index.php
@@ -208,4 +208,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/script.js"></script>
 </body>
-</html>
+</html>p
